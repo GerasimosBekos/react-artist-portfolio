5b8adb46743f939949cb040f9c89491da86ca707
Gerasimos Bekos <gerasimos.bekos@gmail.com>
2025-10-21 21:01:36 -0400

    fix dependancies and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Hello World</h1>} />
+          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/contact" element={<h1>Contact Page</h1>} />
+          <Route path="/gallery" element={<h1>Gallery Page</h1>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
