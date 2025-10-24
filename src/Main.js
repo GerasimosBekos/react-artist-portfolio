c9a6716bf367f0fd562b8250559099cc12c29029
Gerasimos Bekos <gerasimos.bekos@gmail.com>
2025-10-23 20:04:18 -0400

    menu gallery grid

--- src/Main.js
+++ src/Main.js
@@ -57,7 +57,19 @@ function Main(props) {
                             Δημιουργίες
                         </div>
                         <div className="content-gallery-grid">
-                            
+                            <div className="gallery-item wide"><img src="/images/categories/templo.jpg" alt="Τέμπλα" /><span>Τέμπλα</span></div>
+                            <div className="gallery-item tall"><img src="/images/categories/prosk.jpg" alt="Προσκυνητάρια" /><span>Προσκυνητάρια</span></div>
+                            <div className="gallery-item"><img src="/images/categories/stasidia.jpg" alt="Στασίδια" /><span>Στασίδια</span></div>     
+                            <div className="gallery-item tall"><img src="/images/categories/epitafios.jpg" alt="Επιτάφιοι" /><span>Επιτάφιοι</span></div>
+                            <div className="gallery-item"><img src="/images/categories/korniza.jpg" alt="Κορνίζες" /><span>Κορνίζες</span></div>
+                            <div className="gallery-item tall"><img src="/images/categories/stavros.jpg" alt="Σταυροί" /><span>Σταυροί</span></div>
+                             <div className="gallery-item tall"><img src="/images/categories/thronos.jpg" alt="Θρόνοι" /><span>Θρόνοι</span></div>
+                            <div className="gallery-item"><img src="/images/categories/pagkari.jpg" alt="Παγκάρια" /><span>Παγκάρια</span></div>
+                            <div className="gallery-item tall"><img src="/images/categories/polithrona.jpg" alt="Πολυθρόνες" /><span>Πολυθρόνες</span></div>
+                            <div className="gallery-item tall"><img src="/images/categories/amvonas.jpg" alt="Άμβωνες" /><span>Άμβωνες</span></div>
+                            <div className="gallery-item"><img src="/images/categories/karekles.jpg" alt="Καρέκλες" /><span>Καρέκλες</span></div>
+                            <div className="gallery-item"><img src="/images/categories/psaltiri.jpg" alt="Ψαλτήρια" /><span>Ψαλτήρια</span></div>
+                            <div className="gallery-item"><img src="/images/categories/lipsanothiki.jpg" alt="Λειψανοθήκες" /><span>Λειψανοθήκες</span></div>
                         </div>
                     </div>
                 </section>
